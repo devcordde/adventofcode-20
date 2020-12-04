99ba708695d01fdcaa230c77f81ba1896993d456
Felix B <felxbayer@gmail.com>
2020-12-04 19:35:53 +0100

    Complete day 2

--- shared/justentrix/file_reader.py
+++ shared/justentrix/file_reader.py
@@ -5,11 +5,15 @@ inputs_dir = join(dirname(__file__), 'inputs')
 def open_file(file_name):
   return open(f'{inputs_dir}/{file_name}', 'r')
 
-def read_numbers(file_name):
+def read_lines(file_name):
   file = open_file(file_name)
-  nums = []
+  lines = []
 
   for line in file:
-    nums.append(int(line))
-  
-  return nums
+    lines.append(line.replace('\n', ''))
+
+  return lines
+
+def read_numbers(file_name):
+  lines = read_lines(file_name)
+  return list(map(int, lines))
