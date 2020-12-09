a7048ee5664b3d7bc512bff45f97a956f69a609d
NyCodeGHG <nycode@protonmail.com>
2020-12-09 13:15:06 +0100

    Added solution for part 2

--- Day-09/kotlin/NyCodeGHG/solution.kt
+++ Day-09/kotlin/NyCodeGHG/solution.kt
@@ -2,17 +2,24 @@ package de.nycode.aoc2020.day09
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.system.measureTimeMillis
 
 fun main() {
     val numbers = Files.readAllLines(Paths.get("input.txt"))
             .map { it.toLongOrNull() ?: error("Invalid input $it") }
 
-    val invalidNumber = findInvalidNumber(numbers, 25)
-    println("Invalid Number: $invalidNumber")
+    val time = measureTimeMillis {
+        val invalidNumber = findInvalidNumber(numbers, 25) ?: error("Unknown error")
+        println("Invalid Number: $invalidNumber")
+
+        val weakness = findEncryptionWeakness(numbers, invalidNumber)
+        println("Encryption weakness is: $weakness")
+    }
+    println("Took $time ms")
 }
 
 fun findInvalidNumber(numbers: List<Long>, preambleCount: Int): Long? {
-    val data = numbers.subList(preambleCount, numbers.size - 1)
+    val data = numbers.subList(preambleCount, numbers.size)
     val preamble = numbers.take(preambleCount).toMutableList()
 
     var invalidNumber: Long? = null
@@ -37,4 +44,20 @@ fun MutableList<Long>.isCalculable(long: Long): Boolean {
         }
     }
     return false
+}
+
+fun findEncryptionWeakness(numbers: List<Long>, invalidNumber: Long): Long? {
+    numbers.forEachIndexed { index, number ->
+        val contiguous = mutableListOf<Long>()
+        numbers.subList(index, numbers.size).forEach {
+            contiguous.add(it)
+            val sum = contiguous.sum()
+            if (sum > invalidNumber) {
+                return@forEachIndexed
+            } else if (sum == invalidNumber) {
+                return contiguous.minOrNull()!! + contiguous.maxOrNull()!!
+            }
+        }
+    }
+    return null
 }
